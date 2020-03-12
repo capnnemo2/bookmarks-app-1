e5c1b32005232e028178b985ef20c2bd2844f485
Ben Hernandez <benjammin234@gmail.com>
2020-03-12 14:10:47 -0700

    Cleaning up to deploy

--- src/BookmarkItem/BookmarkItem.test.js
+++ src/BookmarkItem/BookmarkItem.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BookmarkItem from './BookmarkItem';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import BookmarkItem from "./BookmarkItem";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<BookmarkItem />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <BrowserRouter>
+      <BookmarkItem />
+    </BrowserRouter>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
   // API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_ENDPOINT: `https://sheltered-savannah-55076.herokuapp.com`,
+  // API_ENDPOINT: `https://sheltered-savannah-55076.herokuapp.com`,
+  API_ENDPOINT: `http://localhost:8000`,
   API_KEY: process.env.API_TOKEN
 };
